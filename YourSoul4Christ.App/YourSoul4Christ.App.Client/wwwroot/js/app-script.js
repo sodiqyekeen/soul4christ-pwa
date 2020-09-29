d0311a58652116ba238b6f58f95d971d13eae2e7
Bro-Zach <olatechlove@gmail.com>
2020-09-29 22:15:29 +0100

    bug/fix invalid chat link in whatsapp share button

--- YourSoul4Christ.App/YourSoul4Christ.App.Client/wwwroot/js/app-script.js
+++ YourSoul4Christ.App/YourSoul4Christ.App.Client/wwwroot/js/app-script.js
@@ -23,6 +23,34 @@ function onBlazorReady() {
         //    }
         //}),
 
+        //waShBtn = function () {
+        //    if (this.isIos === true) {
+        //        var b = [].slice.call(document.querySelectorAll(".wa_btn"));
+        //        for (var i = 0; i < b.length; i++) {
+        //            var t = b[i].getAttribute("data-text");
+        //            var u = b[i].getAttribute("data-href");
+        //            var o = b[i].getAttribute("href");
+        //            var at = "?text=" + encodeURIComponent(t);
+        //            if (t) {
+        //                at += "%20%0A";
+        //            }
+        //            if (u) {
+        //                at += encodeURIComponent(u);
+        //            } else {
+        //                at += encodeURIComponent(document.URL);
+        //            }
+        //            b[i].setAttribute("href", o + at);
+        //            b[i].setAttribute("target", "_top");
+        //            b[i].setAttribute("target", "_top");
+        //            b[i].className += ' activeWhatsapp';
+        //        }
+        //    }
+        //}
+
+        //waShBtn.prototype.isIos = ((navigator.userAgent.match(/Android|iPhone/i) && !navigator.userAgent.match(/iPod|iPad/i)) ? true : false);
+
+        //var theWaShBtn = new waShBtn();
+
 
             /* Top Header */
 
